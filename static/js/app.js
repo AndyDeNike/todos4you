ba25c48c50a3b6ca8396259b068d227fb734b4cf
Andydenike <denikeandrew@gmail.com>
2019-07-29 02:55:59 -0700

    Test

--- static/js/app.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($) {
-  $('.toggle').on('change', function(evt){
-    $(this).parents('form.js-toggle-form').submit()
-  });
-  $('.js-todo-title').on('click', function(evt){
-    $(this).parents('.view').find('form.js-toggle-form').submit()
-  });
-});
